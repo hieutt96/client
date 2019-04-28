a3c0096a766785597b04ef08cf94bf1bf86f9f32
Trung Hieu Tran <hieutt.bka59@gmail.com>
2019-04-28 22:01:41 +0700

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,10 +10,11 @@
 
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-
+		<link rel="stylesheet" type="text/css" href="{{asset('/css/lib/bootstrap.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('/css/lib/font-awesome.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('/css/lib/font-awesome.min.css')}}">
+        <script type="text/javascript" src="{{asset('/js/lib/jquery.js')}}"></script>
+        <script type="text/javascript" src="{{asset('/js/lib/bootstrap.js')}}"></script>
     </head>
     @yield('title')
     <style type="text/css">
